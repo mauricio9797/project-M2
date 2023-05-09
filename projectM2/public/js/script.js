e30b94d4f186add00adb8e3ce0ef4f9cfaec5952
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-05-09 20:19:28 +0200

    Change Breakpoints for Navbar Buttons

--- projectM2/public/js/script.js
+++ projectM2/public/js/script.js
@@ -1,4 +1,22 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("projectM2 JS imported successfully!");
+  // console.log("projectM2 JS imported successfully!");
+  const progressBar = document.querySelector(".progress-bar"); 
+   function startAnimation(){
+    // progressBar.style.width = "0%";
+    progressBar.style.animation = 'none'
+    progressBar.style.animation = 'progressbar-animation 5s linear';
+    // progressBar.style.width = '75%';
+  }
+  
+  function resetAnimation(){
+    progressBar.style.width = '75%';
+    setTimeout(startAnimation, 5000);
+  }
+  
+  setInterval(() => {
+    startAnimation();
+  }, 5000)
 });
+
+
